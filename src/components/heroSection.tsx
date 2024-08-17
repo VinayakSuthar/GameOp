0cc52fa5914ec7185a9618a2cfe577f19484d6c0
Vinayak Suthar <sutharvinayak28@gmail.com>
2024-08-17 21:42:30 +0530

    refactor: Update CSS classes for improved responsiveness and readability

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function HeroSection() {
   return (
     <div className="relative sm:static sm:flex sm:flex-row-reverse items-center justify-between mt-5 mb-10">
-      <div className="hero-image">
+      <div className="hero-image lg:mr-10 xl:mr-0">
         <Image
           src="/images/hero.jpg"
           alt="hero"
@@ -20,7 +20,7 @@ export default function HeroSection() {
         <h2 className="text-3xl sm:text-4xl lg:text-5xl mb-3 sm:mb-7 font-extrabold text-sky-500">
           Aquatic Rescue
         </h2>
-        <p className="text-sm sm:text-base font-medium sm:mb-5 line-clamp-3 lg:line-clamp-none">
+        <p className="text-sm sm:text-base font-medium sm:mb-5 line-clamp-3 xl:line-clamp-none">
           Tickle your brains as your find the best way to rescue these little
           water creatures from their bubbles! Tickle your brains as your find
           the best way to rescue these little water creatures from their
