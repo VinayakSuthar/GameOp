c5b877fcd46d10fb5bfa9eedbf7f73e3e9927c96
Vinayak Suthar <sutharvinayak28@gmail.com>
2024-08-19 13:22:37 +0530

    refactor: Update navigation component to include dynamic links and active link highlighting

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,10 +1,19 @@
-import { Search } from "@mui/icons-material";
+"use client";
 import Link from "next/link";
 import React from "react";
 import SearchBar from "./searchBar";
 import MobileSearch from "./mobileSearch";
+import { usePathname } from "next/navigation";
+import { cn } from "@/utils";
+
+const navLinks = [
+  { href: "/library", text: "Library" },
+  { href: "#", text: "Profile" },
+];
 
 export default function Nav() {
+  const path = usePathname();
+
   return (
     <nav className="flex items-center gap-x-2 sm:gap-x-5 w-full py-5 sm:py-10 text-sm">
       <Link href="/" className="logo">
@@ -14,8 +23,18 @@ export default function Nav() {
         <SearchBar className="w-3/4 lg:w-2/3 hidden sm:block" />
         <MobileSearch />
       </div>
-      <Link href="/library">Library</Link>
-      <Link href="/profile">Profile</Link>
+
+      {navLinks.map((link) => (
+        <Link
+          key={link.href}
+          href={link.href}
+          className={cn({
+            "border-b-2 border-blue-500": path === link.href,
+          })}
+        >
+          {link.text}
+        </Link>
+      ))}
     </nav>
   );
 }
